c4dba43b37b8649e167dfb5ef8dc9e9725fd6bb0
Maksim Sedov <isedovmax@gmail.com>
2021-09-18 13:43:00 +0300

    effective fixes

--- game21.go
+++ game21.go
@@ -6,17 +6,18 @@ import (
 	"time"
 )
 
+// cardSuit - constants for card suits
 type cardSuit string
-type cardValue 	string
-
 const (
-	// cardSuit - constants for card suits
 	spades 			cardSuit 	= 	"spade"
 	hearts 			cardSuit 	= 	"heart"
 	clubs 			cardSuit 	= 	"club"
 	diamonds 		cardSuit 	= 	"diamond"
+)
 
-	// cardValue - constants for card values
+// cardValue - constants for card values
+type cardValue 	string
+const (
 	two 			cardValue 	=	"2"
 	three			cardValue 	= 	"3"
 	four 			cardValue 	= 	"4"
@@ -30,11 +31,18 @@ const (
 	queen			cardValue	=	"Q"
 	king			cardValue	=	"K"
 	ace				cardValue	=	"A"
+)
 
-	// constants for the length of arrays
-	numberOfCards	int			=	52
-	numberOfSuits	int			=	4
-	numberOfValues	int			=	13
+// constants for the length of arrays
+const (
+	// numberOfSuits - length of the suits array
+	numberOfSuits	int	= 4
+
+	// numberOfValues - length of the values array
+	numberOfValues	int	= 13
+
+	// maxScore - maximum number of points in the game
+	maxScore		int	= 21
 )
 
 // the card struct describes the card.
@@ -42,7 +50,7 @@ const (
 type card struct {
 	value 	cardValue
 	suit 	cardSuit
-	cost 	int
+	cost 	int //TODO разделить механики
 }
 
 // the player struct describes the player
@@ -53,20 +61,18 @@ type player struct {
 }
 
 // the score method returns the sum of the player's points
-func (p *player) score() int {
-	var score = 0
+func (p *player) score() (result int) {
 	for _, card := range p.cards {
-		score += card.cost
+		result += card.cost
 	}
-	return score
+	return
 }
 
 // the showCards method returns the values and suits of all the player's cards as a string
-func (p *player) showCards() string {
-	var allCards string
+func (p *player) showCards() (allCards string) {
 	for _, card := range p.cards {
 		if card.value != ""{
-			allCards += string(card.value) + " " + string(card.suit) + ", "
+			allCards += fmt.Sprintf("%s %s, ", card.value, card.suit)
 		}
 	}
 	if allCards == "" {
@@ -92,15 +98,20 @@ func (p *player) takeCard(deck *[]card) {
 // func createDeck generates a deck of cards and returns it as a slice
 func createDeck() []card {
 	// suits - an array of all suits of cards
-	// values - an array of all values of cards
-	// costs - an array of all costs of cards
-	// deck - a slice of a deck of cards
-	// current - a variable for the current card in the deck
 	suits := [numberOfSuits]cardSuit{spades, hearts, clubs, diamonds}
+
+	// values - an array of all values of cards
 	values := [numberOfValues]cardValue{two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace}
+
+	// costs - an array of all costs of cards
 	costs := [numberOfValues]int{2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 11}
-	deck := make([]card, numberOfCards)
-	var current = 0
+
+	// deck - a slice of a deck of cards
+	deck := make([]card, numberOfValues*numberOfSuits)
+
+	// current - a variable for the current card in the deck
+	current := 0
+
 	for v := 0; v < numberOfValues ; v++ {
 		for s := 0; s < numberOfSuits ; s++ {
 			deck[current].value = values[v]
@@ -115,7 +126,9 @@ func createDeck() []card {
 // func randomDeck shuffles the deck of cards
 // input parameter: deck of cards
 func randomDeck(deck []card) {
+	// function for randomness
 	rand.Seed(time.Now().UnixNano())
+
 	for key := range deck {
 		randInt := rand.Intn(len(deck))
 		deck[key], deck[randInt] = deck[randInt], deck[key]
@@ -124,34 +137,56 @@ func randomDeck(deck []card) {
 
 // func computerGame creates a computer player and gives him cards
 // input parameter: deck of cards
-// output parameter: computer player
-func computerGame(deck *[]card) player{
-	// playerAI - the object of the player structure
-	var playerAI player
+// output parameter: computer player, playerAI - the object of the player structure TODO исправить коммент
+func computerGame(deck *[]card) (playerAI player) {
 	playerAI.name = "computer"
+
+	// costs - an array of all costs of cards
+	costs := [numberOfValues]int{2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 11}
+
+	// matchCards - number of matching cards
+	matchCards := 0
+
+	// chance - a chance to take another card
+	chance := 0
+
+	// function for randomness
+	rand.Seed(time.Now().UnixNano())
+
 	for range *deck {
 		if playerAI.score() < 17 {
 			playerAI.takeCard(deck)
+		} else if playerAI.score() < 20 {
+			for _, value := range costs {
+				if value <= maxScore - playerAI.score() {
+					matchCards += 1
+				}
+			}
+			chance = int(float64(matchCards) / float64(numberOfValues) * 100)
+			randInt := 1 + rand.Intn(100)
+			if randInt <= chance {
+				playerAI.takeCard(deck)
+			} else {
+				break
+			}
 		} else {
 			break
 		}
 	}
-	return playerAI
+	return
 }
 
 // func playerGame creates a user player and gives him cards
 // input parameters: deck of cards, name of user
-// output parameter: user player
-func playerGame(deck *[]card, name string) player{
-	// playerUser - the object of the player structure
-	var playerUser player
+// output parameter: user player, playerUser - the object of the player structure TODO комментарий
+func playerGame(deck *[]card, name string) (playerUser player, err error){
 	playerUser.name = name
 
 	if len(*deck) != 0 {
 		playerUser.takeCard(deck)
 		playerUser.info()
 	} else {
-		return playerUser
+		return playerUser, nil
 	}
 
 stop:
@@ -159,7 +194,11 @@ stop:
 		// answer - a variable for the player's response
 		var answer string
 		fmt.Println("more? (Y/N)")
-		fmt.Scanln(&answer)
+		_, err := fmt.Scanln(&answer)
+		if err != nil {
+			fmt.Println(err)
+			return player{}, err
+		}
 		switch answer{
 		case "Y", "y":
 			playerUser.takeCard(deck)
@@ -171,7 +210,7 @@ stop:
 			continue stop
 		}
 	}
-	return playerUser
+	return playerUser, nil
 }
 
 // func result outputs the result of the round to the console
@@ -184,11 +223,11 @@ func result(playerAI player, playerUser player) {
 	playerAI.info()
 	playerUser.info()
 
-	if playerAI.score() > 21 && playerUser.score() > 21 {
+	if playerAI.score() > maxScore && playerUser.score() > maxScore {
 		fmt.Println("everyone lost!")
-	} else if (playerAI.score() > playerUser.score() && playerAI.score() <= 21) || (playerUser.score() > 21) {
+	} else if (playerAI.score() > playerUser.score() && playerAI.score() <= maxScore) || (playerUser.score() > maxScore) {
 		fmt.Println(playerAI.name + " won!")
-	} else if (playerAI.score() < playerUser.score() && playerUser.score() <= 21) || (playerAI.score() > 21) {
+	} else if (playerAI.score() < playerUser.score() && playerUser.score() <= maxScore) || (playerAI.score() > maxScore) {
 		fmt.Println(playerUser.name + " won!")
 	} else if playerAI.score() == playerUser.score() {
 		fmt.Println("draw!")
@@ -203,12 +242,19 @@ func game(deck []card) {
 	var playerAI, playerUser player
 	var name string
 	fmt.Println("enter your name:")
-	fmt.Scanln(&name)
+	_, err := fmt.Scanln(&name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 restart:
 	for range deck {
 		playerAI = computerGame(&deck)
-		playerUser = playerGame(&deck, name)
+		playerUser, err = playerGame(&deck, name)
+		if err != nil {
+			return
+		}
 
 		result(playerAI, playerUser)
 
@@ -221,7 +267,11 @@ restart:
 			// answer - a variable for the player's response
 			var answer string
 			fmt.Println("start the game again? (Y/N)")
-			fmt.Scanln(&answer)
+			_, err := fmt.Scanln(&answer)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 			switch answer {
 			case "Y", "y":
 				continue restart
